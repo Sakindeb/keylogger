852f7c7c70ac2f2b3bdd2a2a10307abad953e8d5
Sakindeb <sakindeborah@gmail.com>
2023-07-23 21:41:04 +0300

    Current progress, the sms is personalised sending

--- app.py
+++ app.py
@@ -3,11 +3,27 @@ import requests
 from flask import Flask, render_template, request
 import africastalking
 
+
 app = Flask(__name__)
 
 # Replace with your Africa's Talking API credentials
 AFRICASTALKING_USERNAME = 'spaceity'
 AFRICASTALKING_API_KEY = 'b07e0b95e54d9747c23eb69011ee85c7c842ac57254e5bc4590bfe683fecff32'
+def initialize_sms():
+    try:
+        # Replace 'YOUR_USERNAME' and 'YOUR_API_KEY' with your actual Africa's Talking credentials
+        africastalking.initialize(username='spaceity', api_key='b07e0b95e54d9747c23eb69011ee85c7c842ac57254e5bc4590bfe683fecff32')
+        return True
+    except Exception as e:
+        print(f"Error initializing Africa's Talking: {e}")
+        return False
+
+# Initialize Africa's Talking SMS
+if initialize_sms():
+    sms = africastalking.SMS
+else:
+    sms = None
+
 
 
 IPINFO_API_KEY = '4a156323e54c50'  # Replace with your API key from https://ipinfo.io/
@@ -54,6 +70,10 @@ def get_ipinfo_details(ip_address):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        # Get user's name and phone number from the form
+        user_name = request.form['name']
+        user_phone = request.form['phone']
+
         # Run netstat -ano command
         cmd_output = run_command('netstat -ano')
 
@@ -66,22 +86,26 @@ def index():
 
         # Extract foreign addresses
         foreign_addresses = extract_foreign_addresses(filtered_output)
+
         # Get IP details and save to another file
         with open('foreign_addresses_details.txt', 'w') as file:
             for ip_address in foreign_addresses:
                 details = get_ipinfo_details(ip_address)
                 file.write(f"{ip_address}: {details}\n")
 
-         # Count the number of loggers
+        # Count the number of loggers
         with open('foreign_addresses_details.txt', 'r') as file:
             num_loggers = sum(1 for line in file)
 
-        # Prepare the SMS message
-        message = f"Hey champ! You have {num_loggers} logger(s) in your computer. Login to Safely to check them out."
+        # Prepare the personalized SMS message
+        message = f"Hey {user_name}! You have {num_loggers} logger(s) in your computer. Login to Safely to check them out."
 
-
-        phone_number = '+254115151539'  # Replace with the recipient's phone number
-        send_sms(phone_number, message)
+        # Send the personalized SMS to the user's phone number
+        try:
+            response = sms.send(message, [user_phone])
+            print(response)
+        except Exception as e:
+            print(f"Error sending SMS: {e}")
 
         return render_template('result.html')
 
